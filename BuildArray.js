975d1e1d298b5b8e72d7af33513c81ad81c6df78
mohdaman7 <muhammedaman2.oaman@gmail.com>
2025-03-15 12:51:45 +0530

    first commit

---
+++ BuildArray.js
@@ -0,0 +1,19 @@
+
+
+let nums = [0,2,1,5,3,4]
+var buildArray = function(nums) {
+    let arr = [];
+    for(let i=0;i<nums.length;i++){
+        let num = nums[nums[i]]
+        arr.push(num)
+    }
+    return arr
+};
+console.log(buildArray(nums))
+
+
+
+
+
+
+
