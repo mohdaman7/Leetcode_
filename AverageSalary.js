a7f97c57b65a3cf070eda46ac8811d4278dc9da9
mohdaman7 <muhammedaman2.oaman@gmail.com>
2025-04-18 11:16:08 +0530

    1491. Average Salary Excluding the Minimum and Maximum Salary

---
+++ AverageSalary.js
@@ -0,0 +1,15 @@
+
+let salary = [4000,3000,1000,2000]
+var average = function(salary) {
+    let sum = 0, len = 0;
+    let min = Math.max(...salary)
+    let max = Math.min(...salary)
+    for(let i=0;i<salary.length;i++){
+        if(salary[i]!==min && salary[i]!==max){
+            sum+=salary[i]
+            len++
+        }
+    }
+    return sum/len
+};
+console.log(average(salary))
