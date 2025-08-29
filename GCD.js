073ecc38203af91782b2a6ee3164ac66e884fe24
mohdaman7 <muhammedaman2.oaman@gmail.com>
2025-08-29 15:35:04 +0530

    3658. GCD of Odd and Even Sums

---
+++ GCD.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+// You are given an integer n. Your task is to compute the GCD (greatest common divisor) of two values:
+
+// sumOdd: the sum of the first n odd numbers.
+
+// sumEven: the sum of the first n even numbers.
+
+// Return the GCD of sumOdd and sumEven.
+
+ 
+
+// Example 1:
+
+// Input: n = 4
+
+// Output: 4
+
+// Explanation:
+
+// Sum of the first 4 odd numbers sumOdd = 1 + 3 + 5 + 7 = 16
+// Sum of the first 4 even numbers sumEven = 2 + 4 + 6 + 8 = 20
+// Hence, GCD(sumOdd, sumEven) = GCD(16, 20) = 4.
+
+
+let n = 4;
+var gcdOfOddEvenSums = function(n) {
+    return n
+};
+console.log(gcdOfOddEvenSums(n))
